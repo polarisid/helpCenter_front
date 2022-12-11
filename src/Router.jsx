fddbfc5435cabd615b301dcb1de421f99e53102c
Daniel Carvalho <38544111+polarisid@users.noreply.github.com>
2022-12-11 12:09:14 -0300

    Initial commit

---
+++ src/Router.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+const PagesRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default PagesRoutes;
